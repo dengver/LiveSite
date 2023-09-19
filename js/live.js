fa33a1fc319a618bcec04a625659b36641aa9d56
dengver <143784709+dengver@users.noreply.github.com>
2023-09-19 13:42:03 +0900

    Add files via upload

---
+++ js/live.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    //toggle
+    function openNav(){
+        document.getElementById("myNav").style.width="100%;"
+    }
+    function closeNav(){
+        document.getElementById("myNav").style.width="0%;"
+    }
+
+    // 팝업
+    $(".btn").on("click",function(){
+        $("#popup").fadeIn();
+    });
+    $(".close").on("click",function(){
+        $("#popup").fadeOut();
+    });
+});
